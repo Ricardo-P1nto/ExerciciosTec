6baed68d27d95454c19ccb664c7def32266711b2
Ricardo-P1nto <157704724+Ricardo-P1nto@users.noreply.github.com>
2024-06-28 11:46:47 +0100

    Add files via upload

--- exercicio-aula1/src/main/java/com/example/exercicio_aula1/controllers/PessoaController.java
+++ exercicio-aula1/src/main/java/com/example/exercicio_aula1/controllers/PessoaController.java
@@ -1,20 +1,35 @@
 package com.example.exercicio_aula1.controllers;
 
 import com.example.exercicio_aula1.Models.Pessoa;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/pessoas")
-public  class PessoaController{
+public class PessoaController {
+    ArrayList<Pessoa> listaPessoas = new ArrayList<>();
+
     @GetMapping("/pessoa")
     public Pessoa gerarPessoa(
             @RequestParam String nome,
             @RequestParam int idade
-    ){
+    ) {
         Pessoa pessoa1 = new Pessoa(nome, idade);
         return pessoa1;
     }
+
+    @PostMapping("/criar")
+    public Pessoa criarPessoa(
+            @RequestBody Pessoa pessoa1
+    ) {
+        listaPessoas.add(pessoa1);
+        return pessoa1;
+    }
+
+    @GetMapping("/listar")
+    public ArrayList<Pessoa> listarPessoas() {
+        return listaPessoas;
+    }
 }
+
